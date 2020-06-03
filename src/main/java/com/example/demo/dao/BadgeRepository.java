bccb00a3c8e4053b700ad69e120add968bec9e6d
Your Name <you@example.com>
2020-06-03 09:56:41 +0530

    hibernate insert get

---
+++ src/main/java/com/example/demo/dao/BadgeRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.pojos.Badge;
+
+
+
+public interface BadgeRepository  extends JpaRepository<Badge,Long>{
+
+}
---
+++ src/main/java/com/example/demo/pojos/Badge.java
@@ -0,0 +1,37 @@
+package com.example.demo.pojos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "badge")
+public class Badge {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long badgeId;
+	
+	public Long getBadgeId() {
+		return badgeId;
+	}
+
+	public void setBadgeId(Long badgeId) {
+		this.badgeId = badgeId;
+	}
+
+	public String getBadgeName() {
+		return badgeName;
+	}
+
+	public void setBadgeName(String badgeName) {
+		this.badgeName = badgeName;
+	}
+
+	@Column(name = "badgename")
+	private String badgeName;
+
+}
