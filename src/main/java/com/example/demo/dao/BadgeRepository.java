87c8a6c5eded169b26ab847b1fb1d598432118d9
Your Name <you@example.com>
2020-06-03 11:38:56 +0530

    hibernate insert get

--- src/main/java/com/example/demo/dao/BadgeRepository.java
+++ src/main/java/com/example/demo/dao/BadgeRepository.java
@@ -1,8 +1,6 @@
 package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.pojos.Badge;
 
